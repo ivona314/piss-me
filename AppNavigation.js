9be29d50f3423e8026de0c2e2bdb7fb7075f4078
LovroColic <lovro.colic.lc@gmail.com>
2020-04-05 21:39:37 +0200

    added drawer navigator

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,13 +1,70 @@
-import { createStackNavigator } from 'react-navigation-stack'
-import Home from "./src/Screens/Home";
+import React, { Component } from 'react'
+import { View, Image, TouchableOpacity } from 'react-native'
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import {createStackNavigator} from 'react-navigation-stack';
+import WelcomeComponent from "./src/Screens/Home";
+import ProfileComponent from "./src/Screens/Profile";
 
-const AppNavigation = createStackNavigator(
-    {
-        Home: { screen: Home }
-    },
-    {
-        initialRouteName: 'Home'
+class NavigationDrawerStructure extends Component {
+    toggleDrawer = () => {
+        this.props.navigationProps.toggleDrawer();
+    };
+    render() {
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
+                    <Image
+                        source={{ uri: 'https://reactnativecode.com/wp-content/uploads/2018/04/hamburger_icon.png' }}
+                        style={{ width: 25, height: 25, marginLeft: 15 }}
+                    />
+                </TouchableOpacity>
+            </View>
+        );
     }
-);
+}
 
-export default AppNavigation
+const FirstActivity_StackNavigator = createStackNavigator({
+    Welcome: {
+        screen: WelcomeComponent,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Home',
+            headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+            headerStyle: {
+                backgroundColor: '#FFF',
+            },
+            headerTintColor: 'black',
+        }),
+    },
+});
+
+const Screen2_StackNavigator = createStackNavigator({
+    Profile: {
+        screen: ProfileComponent,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Profile',
+            headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+            headerStyle: {
+                backgroundColor: '#FFF',
+            },
+            headerTintColor: 'black',
+        }),
+    },
+});
+
+const DrawerNavigatorExample = createDrawerNavigator({
+    WelcomeComponent: {
+        screen: FirstActivity_StackNavigator,
+        navigationOptions: {
+            drawerLabel: 'Home',
+        },
+    },
+    ProfileComponent: {
+        screen: Screen2_StackNavigator,
+        navigationOptions: {
+            drawerLabel: 'Profile',
+        },
+    }
+});
+
+export default createAppContainer(DrawerNavigatorExample);
--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 
-export default class Home extends Component {
+export default class Profile extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Home</Text>
+                <Text>Profile</Text>
             </View>
         )
     }
